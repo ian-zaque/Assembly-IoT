17734c01225488e3b7fef2dad9d10ff16eaf254b
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2022-12-12 22:07:20 -0300

    trying to add Thread for interruption

--- mqtt.c
+++ mqtt.c
@@ -1,15 +1,17 @@
-//gcc mqtt.c -o mqtt.run -lpaho-mqtt3c
+//gcc mqtt.c -o mqtt.run -lpaho-mqtt3c -lwiringPi -pthread -lm -lrt -lcrypt
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 //BIB FOR MQTT
 #include <MQTTClient.h>
 
-//BIB FOR THREAD AND INTERRUPT
+//BIB FOR THREAD, INTERRUPT, GPIO
 #include <wiringPi.h>
+#include <pthread.h>
 
 //BIB FOR DISPLAY
 //#include "display.h"
@@ -18,17 +20,52 @@
 #define USER           "aluno"
 #define PASSWORD       "@luno*123"
 #define CLIENTID       "MQTTClientRASP"
-#define QOS            1
-#define TIMEOUT        10000L
+#define QOS            2
+#define TIMEOUT        4000L
 
 #define NODEMCU_PUBLISH  "NODEMCU_PUBLISH"
 #define NODEMCU_RECEIVE  "NODEMCU_RECEIVE"
 
+#define BTN1 19
+#define BTN2 23
+#define BTN3 25
+
 MQTTClient client;
+
+// Current state of the buttons 1, 2, 3
+static volatile int stateBtn1;
+static volatile int stateBtn2;
+static volatile int stateBtn3;
 
 void publish(MQTTClient client, char* topic, char* payload);
 int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message);
 void evaluateRecData(char *topicName, char *payload);
+
+PI_THREAD (BUTTON_INTERRUPTION){
+  for (;;){
+     stateBtn1 = !digitalRead(BTN1);                     // Get initial state of pin
+     stateBtn2 = !digitalRead(BTN2);                     // Get initial state of pin
+     stateBtn3 = !digitalRead(BTN3); 
+
+     printf("VALUE 1: %d \n \n", stateBtn1);
+     printf("VALUE 2: %d \n \n", stateBtn2);
+     printf("VALUE 2: %d \n \n", stateBtn3);
+     
+	 if (stateBtn1 == 0) {
+		publish(client, NODEMCU_PUBLISH, "30");         //this checks the NodeMCU status.
+	 }
+
+	 if (stateBtn2 == 0) {
+		publish(client, NODEMCU_PUBLISH, "40");         ///this requests analog input value
+	 }
+
+	 if (stateBtn3 == 0) {
+		publish(client, NODEMCU_PUBLISH, "60");         // this turn on the led,
+		sleep(2);                                       // wait 2 seconds
+		publish(client, NODEMCU_PUBLISH, "70");         // then turns off the led
+	 }
+  }
+}
 
 int main(int argc, char *argv[]){
    int rc;
@@ -48,9 +85,15 @@ int main(int argc, char *argv[]){
 
     MQTTClient_subscribe(client, NODEMCU_RECEIVE, 0);
 
-    //PI_THREAD (myThread){
+	wiringPiSetup();                                // Init  -- use the physical pin number on RPi P1 connector
+	pinMode(BTN1, INPUT);                          // Set pin to output in case it's not
+	pinMode(BTN2, OUTPUT);                         // Set pin to output in case it's not
+	pinMode(BTN3, OUTPUT);                         // Set pin to output in case it's not
 
-    //}
+	piThreadCreate(BUTTON_INTERRUPTION);            // CREATING THREAD TO VERIFY INTERRUPTIONS
+	//wiringPiISR(BTN1, INT_EDGE_BOTH, &handle);     // Bind to interrupt	
+	//wiringPiISR(BTN2, INT_EDGE_BOTH, &handle);     // Bind to interrupt
+    //wiringPiISR(BTN3, INT_EDGE_BOTH, &handle);     // Bind to interrupt
 
     system("cls || clear");
     printf("PBL - Interfaces de E/S. \n \n");
@@ -80,16 +123,17 @@ int main(int argc, char *argv[]){
 
             if(i == 2){                   //this requests some digital input value
                //system("cls || clear");
-               printf("Ação: Solicita o valor de uma das entradas digitais. \n \n");
+               printf("Ação: Solicita o valor de uma das entradas digitais. \n");
                sleep(1);
-               
-               for (int sensorIdx = 50; sensorIdx < 59; sensorIdx++){   //LOOP TO COMMUTE DIGITAL SENSORS
+
+               //LOOP TO COMMUTE DIGITAL SENSORS REQUESTS
+               for (int sensorIdx = 50; sensorIdx < 59; sensorIdx++){
                    sleep(2);
                    printf("Entrada digital: %d. \n \n", sensorIdx);
-                   
+
                    char numStr[6] = "";
                    sprintf(numStr, "%d", sensorIdx);               //CONVERT INTEGER TO STRING
-                   
+
                    publish(client, NODEMCU_PUBLISH, numStr);
                }
             }
@@ -124,15 +168,16 @@ int main(int argc, char *argv[]){
 
 //SEND MESSAGE
 void publish(MQTTClient client, char* topic, char* payload) {
+     //sleep(3);
      MQTTClient_message pubmsg = MQTTClient_message_initializer;
 
      pubmsg.payload = payload;
      pubmsg.payloadlen = strlen(pubmsg.payload);
-     pubmsg.qos = 2;
+     pubmsg.qos = QOS;
      pubmsg.retained = 0;
      MQTTClient_deliveryToken token;
      MQTTClient_publishMessage(client, topic, &pubmsg, &token);
-     MQTTClient_waitForCompletion(client, token, 1000L);
+     MQTTClient_waitForCompletion(client, token, TIMEOUT);
 
      printf("Mensagem enviada! \n\rTopico: %s Mensagem: %s\n", topic, payload);
 }
@@ -150,7 +195,7 @@ int on_message(void *context, char *topicName, int topicLen, MQTTClient_message 
 }
 
 void evaluateRecData(char * topicName, char *payload){
-     
+
     if(strcmp(topicName, "NODEMCU_RECEIVE") == 0){
         // 1F
         if (payload[0] == '1' && payload[1] == 'F'){
@@ -168,7 +213,7 @@ void evaluateRecData(char * topicName, char *payload){
         else if (payload[0] == '0' && payload[1] == '1'){
            char inputValue[] = "";
            strncpy(inputValue, payload + 2, sizeof(payload) - 2);       // COPYING ONLY THE INPUT VALUE RECEIVED
-           
+
            printf("Resposta 01: %s. \n", inputValue);
         }
 
@@ -178,7 +223,7 @@ void evaluateRecData(char * topicName, char *payload){
            char inputValue[] = "";
            strncpy(digitalSensor, payload + 2, sizeof(payload) - 2);       // COPYING ONLY THE DIGITAL SENSOR RECEIVED
            strncpy(inputValue, payload + 4, sizeof(payload) - 4);       // COPYING ONLY THE INPUT VALUE RECEIVED
-           
+
            printf("Resposta 02: %s , %s. \n", digitalSensor, inputValue);
         }
 
@@ -191,5 +236,4 @@ void evaluateRecData(char * topicName, char *payload){
     else if(strcmp(topicName, "NODEMCU_PUBLISH") == 0){
          printf("Resposta NODEMCU_PUBLISH: %s. \n", payload);
     }
-
 }
